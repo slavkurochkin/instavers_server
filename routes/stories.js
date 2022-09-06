a87619039677b4faafac4ec1ba39c7e41a1e81a2
Slav Kurochkin <slav@Slavs-MacBook-Pro-3.local>
2022-09-05 22:36:58 -0400

    make it better

---
+++ routes/stories.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { getStories, createStory, updateStory, deleteStory, likeStory } from "../controlers/stories.js";
+const router = Router();
+import authentication from '../midlewares/authentication.js'
+
+router.get("/", getStories);
+router.post("/", authentication, createStory);
+router.patch("/:id", authentication, updateStory);
+router.delete("/:id", authentication, deleteStory);
+router.patch("/:id/likeStory", authentication, likeStory);
+
+export default router;
